95a978317bc3fdd2f0a90f5b05412012a558678f
RobertMN076 <robert.giulio@escolar.ifrn.edu.br>
2024-09-08 21:03:14 -0300

    slide-de-aparecida

---
+++ code.js
@@ -0,0 +1,265 @@
+// Selecionando o botão que aciona o modal (no caso, a imagem)
+const image = document.querySelector('img[data-toggle="modal"]');
+
+// Selecionando o modal
+const modal = document.getElementById('myModal');
+
+// Adicionando um evento de clique à imagem
+image.addEventListener('click', () => {
+  modal.classList.add('show');
+  modal.style.display = 'block';
+  modal.style.opacity = 1;
+});
+
+// Adicionando um evento de clique ao botão de fechar
+const closeButton = modal.querySelector('.close');
+closeButton.addEventListener('click', () => {
+  modal.classList.remove('show');
+  modal.style.display = 'none';
+  modal.style.opacity = 0;
+});
+
+
+// modal 2
+
+
+// Selecionando o botão que aciona o modal (no caso, a imagem)
+const image2 = document.querySelector('img[data-toggle="modal2"]');
+
+// Selecionando o modal
+const modal2 = document.getElementById('modal2');
+
+// Adicionando um evento de clique à imagem
+image2.addEventListener('click', () => {
+  modal2.classList.add('show');
+  modal2.style.display = 'block';
+  modal2.style.opacity = 1;
+});
+
+// Adicionando um evento de clique ao botão de fechar
+const closeButton2 = modal2.querySelector('.close');
+closeButton2.addEventListener('click', () => {
+  modal2.classList.remove('show');
+  modal2.style.display = 'none';
+  modal2.style.opacity = 0;
+});
+
+// modal 3
+
+const image3 = document.querySelector('img[data-toggle="modal3"]');
+
+// Selecionando o modal
+const modal3 = document.getElementById('modal3');
+
+// Adicionando um evento de clique à imagem
+image3.addEventListener('click', () => {
+  modal3.classList.add('show');
+  modal3.style.display = 'block';
+  modal3.style.opacity = 1;
+});
+
+// Adicionando um evento de clique ao botão de fechar
+const closeButton3 = modal3.querySelector('.close');
+closeButton3.addEventListener('click', () => {
+  modal3.classList.remove('show');
+  modal3.style.display = 'none';
+  modal3.style.opacity = 0;
+});
+
+// modal 4
+
+const image4 = document.querySelector('img[data-toggle="modal4"]');
+
+// Selecionando o modal
+const modal4 = document.getElementById('modal4');
+
+// Adicionando um evento de clique à imagem
+image4.addEventListener('click', () => {
+  modal4.classList.add('show');
+  modal4.style.display = 'block';
+  modal4.style.opacity = 1;
+});
+
+// Adicionando um evento de clique ao botão de fechar
+const closeButton4 = modal4.querySelector('.close');
+closeButton4.addEventListener('click', () => {
+  modal4.classList.remove('show');
+  modal4.style.display = 'none';
+  modal4.style.opacity = 0;
+});
+
+//modal 5
+
+const image5 = document.querySelector('img[data-toggle="modal5"]');
+
+// Selecionando o modal
+const modal5 = document.getElementById('modal5');
+
+// Adicionando um evento de clique à imagem
+image5.addEventListener('click', () => {
+  modal5.classList.add('show');
+  modal5.style.display = 'block';
+  modal5.style.opacity = 1;
+});
+
+// Adicionando um evento de clique ao botão de fechar
+const closeButton5 = modal5.querySelector('.close');
+closeButton5.addEventListener('click', () => {
+  modal5.classList.remove('show');
+  modal5.style.display = 'none';
+  modal5.style.opacity = 0;
+});
+
+// modal 6
+
+const image6 = document.querySelector('img[data-toggle="modal6"]');
+
+// Selecionando o modal
+const modal6 = document.getElementById('modal6');
+
+// Adicionando um evento de clique à imagem
+image6.addEventListener('click', () => {
+  modal6.classList.add('show');
+  modal6.style.display = 'block';
+  modal6.style.opacity = 1;
+});
+
+// Adicionando um evento de clique ao botão de fechar
+const closeButton6 = modal6.querySelector('.close');
+closeButton6.addEventListener('click', () => {
+  modal6.classList.remove('show');
+  modal6.style.display = 'none';
+  modal6.style.opacity = 0;
+});
+
+// modal 7
+
+const image7 = document.querySelector('img[data-toggle="modal7"]');
+
+// Selecionando o modal
+const modal7 = document.getElementById('modal7');
+
+// Adicionando um evento de clique à imagem
+image7.addEventListener('click', () => {
+  modal7.classList.add('show');
+  modal7.style.display = 'block';
+  modal7.style.opacity = 1;
+});
+
+// Adicionando um evento de clique ao botão de fechar
+const closeButton7 = modal7.querySelector('.close');
+closeButton7.addEventListener('click', () => {
+  modal7.classList.remove('show');
+  modal7.style.display = 'none';
+  modal7.style.opacity = 0;
+});
+
+// modal 8
+
+const image8 = document.querySelector('img[data-toggle="modal8"]');
+
+// Selecionando o modal
+const modal8 = document.getElementById('modal8');
+
+// Adicionando um evento de clique à imagem
+image8.addEventListener('click', () => {
+  modal8.classList.add('show');
+  modal8.style.display = 'block';
+  modal8.style.opacity = 1;
+});
+
+// Adicionando um evento de clique ao botão de fechar
+const closeButton8 = modal8.querySelector('.close');
+closeButton8.addEventListener('click', () => {
+  modal8.classList.remove('show');
+  modal8.style.display = 'none';
+  modal8.style.opacity = 0;
+});
+
+// modal 9
+
+const image9 = document.querySelector('img[data-toggle="modal9"]');
+
+// Selecionando o modal
+const modal9 = document.getElementById('modal9');
+
+// Adicionando um evento de clique à imagem
+image9.addEventListener('click', () => {
+  modal9.classList.add('show');
+  modal9.style.display = 'block';
+  modal9.style.opacity = 1;
+});
+
+// Adicionando um evento de clique ao botão de fechar
+const closeButton9 = modal9.querySelector('.close');
+closeButton9.addEventListener('click', () => {
+  modal9.classList.remove('show');
+  modal9.style.display = 'none';
+  modal9.style.opacity = 0;
+});
+
+// modal 10 
+
+const image10 = document.querySelector('img[data-toggle="modal10"]');
+
+// Selecionando o modal
+const modal10 = document.getElementById('modal10');
+
+// Adicionando um evento de clique à imagem
+image10.addEventListener('click', () => {
+  modal10.classList.add('show');
+  modal10.style.display = 'block';
+  modal10.style.opacity = 1;
+});
+
+// Adicionando um evento de clique ao botão de fechar
+const closeButton10 = modal10.querySelector('.close');
+closeButton10.addEventListener('click', () => {
+  modal10.classList.remove('show');
+  modal10.style.display = 'none';
+  modal10.style.opacity = 0;
+});
+
+// modal 11
+
+const image11 = document.querySelector('img[data-toggle="modal11"]');
+
+// Selecionando o modal
+const modal11 = document.getElementById('modal11');
+
+// Adicionando um evento de clique à imagem
+image11.addEventListener('click', () => {
+  modal11.classList.add('show');
+  modal11.style.display = 'block';
+  modal11.style.opacity = 1;
+});
+
+// Adicionando um evento de clique ao botão de fechar
+const closeButton11 = modal11.querySelector('.close');
+closeButton11.addEventListener('click', () => {
+  modal11.classList.remove('show');
+  modal11.style.display = 'none';
+  modal11.style.opacity = 0;
+});
+
+// modal 12
+
+const image12 = document.querySelector('img[data-toggle="modal12"]');
+
+// Selecionando o modal
+const modal12 = document.getElementById('modal12');
+
+// Adicionando um evento de clique à imagem
+image12.addEventListener('click', () => {
+  modal12.classList.add('show');
+  modal12.style.display = 'block';
+  modal12.style.opacity = 1;
+});
+
+// Adicionando um evento de clique ao botão de fechar
+const closeButton12 = modal12.querySelector('.close');
+closeButton12.addEventListener('click', () => {
+  modal12.classList.remove('show');
+  modal12.style.display = 'none';
+  modal12.style.opacity = 0;
+});
